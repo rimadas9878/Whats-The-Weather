490a4b7bd83841c34af67d868e5b2346a7ca9ed5
Rima Das <rimadas.automationtester@gmail.com>
2022-09-09 22:31:36 -0700

    succussful working code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,23 @@
 var APIKey = 'c5f22b24220c2b11282d984a9ed0e2a0';
+var store = eval(localStorage.cities) || [];
+
+const handleHistory = city => {
+    document.getElementById('cityName').value = city;
+    getWeatherForcast();
+};
+
+const showHistory = () => {
+    document.getElementById('history').innerHTML = '';
+
+    store.forEach(city => {
+        document.getElementById('history').innerHTML += `<button onclick="handleHistory('${city}')">${city}</button>`;
+    });
+};
+
+showHistory();
 
 var searchCityBtn = document.getElementById('searchBtn');
 searchCityBtn.addEventListener('click', getWeatherForcast);
-var days = 5;
 
 
 function getWeatherForcast() {
@@ -12,37 +27,48 @@ function getWeatherForcast() {
         alert("Enter name of the city to Search")
     }
     else {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="
-            + city
-            + "&units=metric&appid="
-            + APIKey)
+        displayPerDayWeather();
+        
+    }
 
-            //console.log(url)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data);
+function displayPerDayWeather(){
 
-                const { name } = data;
-                const { temp } = data.main;
-                const { humidity } = data.main;
-                const { speed } = data.wind;
-                const { icon } = data.weather[0];
-                const { lat } = data.coord;
-                const { lon } = data.coord;
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="
+    + city
+    + "&units=metric&appid="
+    + APIKey)
 
-                console.log(name, temp, humidity, speed, icon, lat, lon);
+    //console.log(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
 
-                document.getElementById('nameOfCity').innerText = name;
-                document.getElementById('iconOfWeather').src = `http://openweathermap.org/img/wn/${icon}.png`
-                document.getElementById('tempDisplay').innerText = temp;
-                document.getElementById('windDisplay').innerText = speed;
-                document.getElementById('humidityDisplay').innerText = humidity;
-                document.getElementById('UVindex').innerText;
+        if(!store.includes(city)) {
+            store.push(city);
+            localStorage.cities = JSON.stringify(store);
+            showHistory();
+        }; 
 
+        const { name } = data;
+        const { temp } = data.main;
+        const { humidity } = data.main;
+        const { speed } = data.wind;
+        const { icon } = data.weather[0];
+        const { lat } = data.coord;
+        const { lon } = data.coord;
 
-                fetch("https://api.openweathermap.org/data/2.5/forecast?lat="
+        console.log(name, temp, humidity, speed, icon, lat, lon);
+
+        document.getElementById('nameOfCity').innerText = name;
+        document.getElementById('iconOfWeather').src = `http://openweathermap.org/img/wn/${icon}.png`
+        document.getElementById('tempDisplay').innerText = temp;
+        document.getElementById('windDisplay').innerText = speed;
+        document.getElementById('humidityDisplay').innerText = humidity;
+        document.getElementById('UVindex').innerText;
+
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat="
                     + lat
                     + "&lon="
                     + lon
@@ -53,68 +79,23 @@ function getWeatherForcast() {
                     })
                     .then(function (data) {
                         console.log(data);
+                        //37.5483 -121.9886
 
+                        for(var i=0, j=1; i<40;i=i+8, j++){
+                            var { dt } = data.list[i];
+                            var { icon } = data.list[i].weather[0];
+                            var { temp } = data.list[i].main;
+                            var { speed } = data.list[i].wind;
+                            var { humidity } = data.list[i].main;
+                            console.log(dt, icon, temp, speed, humidity)
+                            document.getElementById('getDay'+j+'Date').innerText = new Date(dt*1000).toDateString();
+                            document.getElementById('day'+j+'Image').src = `http://openweathermap.org/img/wn/${icon}.png`;
+                            document.getElementById('day'+j+'TemparatureDisplay').innerText = temp;
+                            document.getElementById('day'+j+'WindDisplay').innerText = speed;
+                            document.getElementById('day'+j+'HumidityDisplay').innerText = humidity;
 
-                        var { dt_txt } = data.list[0];
-                        var { icon } = data.list[0].weather[0];
-                        var { temp } = data.list[0].main;
-                        var { speed } = data.list[0].wind;
-                        var { humidity } = data.list[0].main;
-                        console.log(dt_txt, icon, temp, speed, humidity)
-                        document.getElementById('getDay1Date').innerText = dt_txt;
-                        document.getElementById('day1Image').src = `http://openweathermap.org/img/wn/${icon}.png`;
-                        document.getElementById('day1TemparatureDisplay').innerText = temp;
-                        document.getElementById('day1WindDisplay').innerText = speed;
-                        document.getElementById('day1HumidityDisplay').innerText = humidity;
-
-
-                        var { dt_txt } = data.list[8];
-                        var { icon } = data.list[8].weather[0];
-                        var { temp } = data.list[8].main;
-                        var { speed } = data.list[8].wind;
-                        var { humidity } = data.list[8].main;
-                        console.log(dt_txt, temp, speed, humidity)
-                        document.getElementById('getDay2Date').innerText = dt_txt;
-                        document.getElementById('day2Image').src = `http://openweathermap.org/img/wn/${icon}.png`;
-                        document.getElementById('day2TemparatureDisplay').innerText = temp;
-                        document.getElementById('day2WindDisplay').innerText = speed;
-                        document.getElementById('day2HumidityDisplay').innerText = humidity;
-
-                        var { dt_txt } = data.list[16];
-                        var { icon } = data.list[16].weather[0];
-                        var { temp } = data.list[16].main;
-                        var { speed } = data.list[16].wind;
-                        var { humidity } = data.list[16].main;
-                        console.log(dt_txt, temp, speed, humidity)
-                        document.getElementById('getDay3Date').innerText = dt_txt;
-                        document.getElementById('day3Image').src = `http://openweathermap.org/img/wn/${icon}.png`;
-                        document.getElementById('day3TemparatureDisplay').innerText = temp;
-                        document.getElementById('day3WindDisplay').innerText = speed;
-                        document.getElementById('day3HumidityDisplay').innerText = humidity;
-
-                        var { dt_txt } = data.list[24];
-                        var { icon } = data.list[24].weather[0];
-                        var { temp } = data.list[24].main;
-                        var { speed } = data.list[24].wind;
-                        var { humidity } = data.list[24].main;
-                        console.log(dt_txt, temp, speed, humidity)
-                        document.getElementById('getDay4Date').innerText = dt_txt;
-                        document.getElementById('day4Image').src = `http://openweathermap.org/img/wn/${icon}.png`;
-                        document.getElementById('day4TemparatureDisplay').innerText = temp;
-                        document.getElementById('day4WindDisplay').innerText = speed;
-                        document.getElementById('day4HumidityDisplay').innerText = humidity;
-
-                        var { dt_txt } = data.list[32];
-                        var { icon } = data.list[32].weather[0];
-                        var { temp } = data.list[32].main;
-                        var { speed } = data.list[32].wind;
-                        var { humidity } = data.list[32].main;
-                        console.log(dt_txt, temp, speed, humidity)
-                        document.getElementById('getDay5Date').innerText = dt_txt;
-                        document.getElementById('day5Image').src = `http://openweathermap.org/img/wn/${icon}.png`;
-                        document.getElementById('day5TemparatureDisplay').innerText = temp;
-                        document.getElementById('day5WindDisplay').innerText = speed;
-                        document.getElementById('day5HumidityDisplay').innerText = humidity;
+                        }
+                        
 
                         fetch("http://api.openweathermap.org/data/2.5/uvi?appid="
                             + APIKey
@@ -158,5 +139,7 @@ function getWeatherForcast() {
 
             })
 
-    }
+
 }
+}
+
--- README.md
+++ README.md
@@ -1,100 +1,23 @@
-# 06 Server-Side APIs: Weather Dashboard
+<h1>Assignment03-Whats-The-Weatherd</h1>
+<p>This assignment is about fetching current and, 5 days weather data from across the world from an API</p>
 
-## Your Task
+<h3>On the screen you will see</h3>
+<ol>
+<li>When you enter a city name in "Enter the city name" search box, then you will see current and 5 days weather data</li>
+<li>In the top right panel, you will see temparature, Wind speed, humidity and UV Index of current day</li>
+<li>The UV Index will display Green if its low, moderate if its Yellow, Orange if its High, Red if its Very High and Purple if its Extremly High</li>
+<li>Also the icon of weather is displays</li>
+<li>The bottom right panel displays 5 days weather</li>
+<li>The search city is saved</li>
+<li>When you click on the saved city, you will be able to see the weather of that city again</li>
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+</ol>
 
-Use the [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+<h3>Link of Github</h3>
+<p>https://github.com/rimadas9878/Assignment06-Whats-The-Weather</p>
 
-## User Story
+<h3>Link of Website</h3>
+<p>https://rimadas9878.github.io/Assignment06-Whats-The-Weather/</p>
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
-
-## Acceptance Criteria
-
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
-
-## Mock-Up
-
-The following image shows the web application's appearance and functionality:
-
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
-
-## Grading Requirements
-
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+<h3>Image of the prompt messages</h3>
+<img src="./Assets/Images/Weather_Image.png" alt="Weather"/>
